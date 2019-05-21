7b00f0c075d71de43d0de4c7f14a05fc18cf99a9
liabe <ali.ramdani.dz@viacesi.fr>
2019-05-21 13:17:27 +0100

    phase3

--- jpu2016_hero/src/jpu2016/hit/Hit.java
+++ jpu2016_hero/src/jpu2016/hit/Hit.java
@@ -4,22 +4,57 @@ public class Hit {
 	private final int			strength;
 	private final int			range;
 	private final TypeHit	typeHit;
+	
+	private  int MAX_STRENGTH=100 ;
+	private int MIN_STRENGTH = 0; 
+	private  int MAX_RANGE=10 ;
+	private int MIN_RANGE=0;
+
 
 	public Hit(final int strength, final int range, final TypeHit typeHit) {
 		this.strength = strength;
 		this.range = range;
 		this.typeHit = typeHit;
-	}
+		
+	}//hit
 
-	public int getStrength() {
+	public int getStrength() throws Exception {
+		if ((strength < MIN_STRENGTH) || (strength > MAX_STRENGTH)) {
+			throw new Exception("Strength out of range");
+			}
 		return this.strength;
 	}
 
-	public int getRange() {
+	public int getRange() throws Exception {
+
+		if ((range < MIN_RANGE) || (range > MAX_RANGE)) {
+			throw new Exception("Range out of range");
+			}
 		return this.range;
+		
 	}
 
 	public TypeHit getTypeHit() {
 		return this.typeHit;
 	}
-}
+	
+	/**
+	 * @return the mAX_STRENGTH
+	 * @throws Exception 
+	 */
+	public int getMAX_STRENGTH() throws Exception {
+		return MAX_STRENGTH ;
+
+	}
+
+	/**
+	 * @return the mAX_RANGE
+	 * @throws Exception 
+	 */
+	public int getMAX_RANGE() throws Exception {
+		return MAX_RANGE;		
+
+	}
+	
+	
+}//hit
---
+++ jpu2016_hero/test/jpu2016/hit/HitTest.java
@@ -0,0 +1,93 @@
+package jpu2016.hit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HitTest {
+	
+	private Hit hit ; 
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		this.hit = new Hit (1,10,TypeHit.NORMAL);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testGetStrength() throws Exception {
+		final int expected = 1 ; 
+		assertEquals(expected, hit.getStrength());
+	}
+
+	@Test
+	public final void testGetRange() throws Exception {
+		final int expected = 10 ; 
+		assertEquals(expected, hit.getRange());
+	}
+
+	@Test
+	public final void testGetTypeHit() {
+		final TypeHit expected = TypeHit.NORMAL ; 
+		assertEquals(expected, hit.getTypeHit());
+	}
+
+	@Test
+	public final void exceptStrengthMaxStrength()	{
+		try {
+			new Hit (101, 10,TypeHit.NORMAL);
+		}catch (final Exception e ) {
+			final String expected  = "Strength out of strength";
+			assertEquals(expected, e.getMessage());
+		}
+		
+	}
+	@Test
+	public final void exceptStrengthMinStrength()	{
+		try {
+			new Hit (-1 , 10,TypeHit.NORMAL);
+		}catch (final Exception e ) {
+			final String expected  = "Strength out of strength";
+			assertEquals(expected, e.getMessage());
+		}
+		
+	}
+	
+	
+	@Test
+	public final void exceptRangeMaxRange()	{
+		try {
+			new Hit (1,11,TypeHit.NORMAL);
+		}catch (final Exception e ) {
+			final String expected  = "Range out of range";
+			assertEquals(expected, e.getMessage());
+		}
+		
+	}
+	@Test
+	public final void exceptRangeMinRange()	{
+		try {
+			new Hit (1,0,TypeHit.NORMAL);
+		}catch (final Exception e ) {
+			final String expected  = "Range out of range";
+			assertEquals(expected, e.getMessage());
+		}
+		
+	}
+
+}
